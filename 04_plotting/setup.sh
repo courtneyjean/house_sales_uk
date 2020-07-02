f2927ac92c4a96447133560f84108ed4e3c9fa6a
Courtney Irwin <courtneyirwin@Courtneys-MacBook-Pro.local>
2020-07-02 20:58:09 +0100

    added files for heroku deployment

---
+++ 04_plotting/setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"courtney_irwin@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
