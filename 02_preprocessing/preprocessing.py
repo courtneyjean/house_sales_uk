51cb9ab8810c1c4178b08ae434b46fe61009b73e
Courtney Irwin <courtneyirwin@Courtneys-MacBook-Pro.local>
2020-07-07 19:27:23 +0100

    utalising chunking in preprocessing script

--- 02_preprocessing/preprocessing.py
+++ 02_preprocessing/preprocessing.py
@@ -8,7 +8,7 @@ def preprocess_data(pc_list):
 
     df = pd.DataFrame()
 
-    chunksize = 300
+    chunksize = 3000
 
     for chunk in pd.read_csv(DATAFILE, header=None, low_memory=False, chunksize=chunksize):
         ch_time1 = time.time()
@@ -18,7 +18,9 @@ def preprocess_data(pc_list):
             pc_df = pd.DataFrame()
             pc_df = chunk[chunk[13].str.contains(pc, na=False)].copy() #filter only postcodes of interest
             df = pd.concat([df, pc_df])
+    print(df.head())
     df = df.rename(columns=HEADINGS)
+    print(df.head())
     df["year_of_transfer"] = df["date_of_transfer"].apply(lambda x: x.split('-')[0])  # date of transfer
     df.to_csv('/Users/courtneyirwin/Documents/GITREPO/House_Sales_UK/01_data/pp-complete-preprocessed-ALL.csv'.format(pc))
     end = time.time()
